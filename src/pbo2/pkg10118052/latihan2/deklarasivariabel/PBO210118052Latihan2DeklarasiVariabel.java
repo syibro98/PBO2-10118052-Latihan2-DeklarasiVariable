0c0b55e01e8311250dc76ab11b4cfeb981525cda
syibro98 <syibrobagus@gmail.com>
2019-09-23 13:02:46 +0700

    upload

---
+++ src/pbo2/pkg10118052/latihan2/deklarasivariabel/PBO210118052Latihan2DeklarasiVariabel.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118052.latihan2.deklarasivariabel;
+
+/**
+ * @author 
+ * NAMA      : Bagus Syibro Malisi
+ * KELAS     : PBO2
+ * NIM       : 10118052
+ * Deskripsi Program : Latihan ini, untuk menampilkan variabel
+ */
+public class PBO210118052Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilakan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika );
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+    }
+    
+}
